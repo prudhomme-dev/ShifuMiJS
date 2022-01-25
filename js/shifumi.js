943b01810a711ae4954058d3981163c4e41afc4d
cedricoufr <60824937+cedricoufr@users.noreply.github.com>
2022-01-25 10:21:52 +0100

    Level Easy terminée - Réorganisation en fonction pour gérer les niveaux de difficultés

--- js/shifumi.js
+++ js/shifumi.js
@@ -18,6 +18,9 @@ document.querySelector(".playerName").innerHTML = document.querySelector(".playe
 document.querySelector(".scoreIA").innerHTML = scoreIA
 document.querySelector(".playerScore").innerHTML = playerScore
 
+// taux de réussite du l'IA (de 50 à 100)
+let levelTwoIARate = 70
+
 const changeName = (e) => {
     if (e.key == "Enter") {
         if (document.querySelector("#playerName").value != "") {
@@ -56,19 +59,36 @@ const playerChoice = (e) => {
         buttons[i].disabled = true
         if (buttons[i].classList.length == 2) buttons[i].classList.add("noselect")
     }
+    // Lancement du jeu selon le niveau
+    let game
+    let choiceLevel = document.querySelector("#level").value
+    if (choiceLevel==1) game=gameLevelOne()
+    else if (choiceLevel==2) console.log("Niveau 2")
+    else if (choiceLevel==3) console.log("Niveau 3")
+    calculScore(game)
+}
 
-    // Déclenchement du jeu
-    let gagnant = 0
+const gameLevelOne = () => {
+    let winner = 0
     let choiceIA = random(1, 1, 3)
-    if (choicePlayer == choiceIA) gagnant = 0
-    else if (choicePlayer == 1 && choiceIA == 2) gagnant = 1
-    else if (choicePlayer == 1 && choiceIA == 3) gagnant = 2
-    else if (choicePlayer == 2 && choiceIA == 1) gagnant = 2
-    else if (choicePlayer == 2 && choiceIA == 3) gagnant = 1
-    else if (choicePlayer == 3 && choiceIA == 1) gagnant = 1
-    else if (choicePlayer == 3 && choiceIA == 2) gagnant = 2
+    if (choicePlayer == choiceIA) winner = 0
+    else if (choicePlayer == 1 && choiceIA == 2) winner = 1
+    else if (choicePlayer == 1 && choiceIA == 3) winner = 2
+    else if (choicePlayer == 2 && choiceIA == 1) winner = 2
+    else if (choicePlayer == 2 && choiceIA == 3) winner = 1
+    else if (choicePlayer == 3 && choiceIA == 1) winner = 1
+    else if (choicePlayer == 3 && choiceIA == 2) winner = 2
 
-    switch (choiceIA) {
+    displayChoiceIA(choiceIA)
+    return winner
+}
+
+
+
+
+const displayChoiceIA = (choice) =>
+{
+    switch (choice) {
         case 1:
             document.querySelector(".choiceIA").innerHTML = "Pierre"
             break;
@@ -80,9 +100,12 @@ const playerChoice = (e) => {
             break;
     }
 
+}
+
+const calculScore = (winner) => {
     // Calcul des scores
     let winnerText = ""
-    switch (gagnant) {
+    switch (winner) {
         case 0:
             winnerText = "Egalité. Personne ne gagne de point."
             break;
@@ -97,6 +120,7 @@ const playerChoice = (e) => {
     }
     document.querySelector(".winner").innerHTML = winnerText
     setTimeout(updateScore, 2000)
+
 }
 
 const updateScore = () => {
@@ -108,9 +132,11 @@ const updateScore = () => {
         myButtons[i].classList.remove("select")
         myButtons[i].classList.remove("noselect")
     }
+    document.querySelector(".choiceIA").innerHTML=""
+    document.querySelector(".choicePlayer").innerHTML=""
 }
 
-const messageBox = (type = "info", title = "", message = "", reponses = ["OK"], fonction=["closeModal"]) => {
+const messageBox = (type = "info", title = "", message = "", reponses = ["OK"], fonction = ["closeModal"]) => {
     switch (type) {
         case "info":
             document.querySelector(".messageBox").style.backgroundColor = "var(--color-secondary)"
@@ -123,17 +149,16 @@ const messageBox = (type = "info", title = "", message = "", reponses = ["OK"], 
 
         case "question":
             document.querySelector(".messageBox").style.backgroundColor = "var(--color-question)"
-            
+
             break;
     }
-    for (let i=0; i<reponses.length; ++i)
-            {
-                newButton = document.createElement("button")
-                let reponsesTag = document.querySelector(".reponses")
-                newButton.innerHTML=reponses[i]
-                //newButton.addEventListener("click", fonction[i])
-                reponsesTag.appendChild(newButton)
-            }
+    for (let i = 0; i < reponses.length; ++i) {
+        newButton = document.createElement("button")
+        let reponsesTag = document.querySelector(".reponses")
+        newButton.innerHTML = reponses[i]
+        //newButton.addEventListener("click", fonction[i])
+        reponsesTag.appendChild(newButton)
+    }
     document.querySelector(".messageBox h3").innerHTML = title
     document.querySelector(".messageBox p").innerHTML = message
 
@@ -142,9 +167,8 @@ const messageBox = (type = "info", title = "", message = "", reponses = ["OK"], 
 
 }
 
-const closeModal = () =>
-{
-    document.querySelector(".messageBox").style.display="none"
+const closeModal = () => {
+    document.querySelector(".messageBox").style.display = "none"
 }
 
 //messageBox("", "Mon information", "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima deserunt cumque necessitatibus nemo velit iure, similique possimus accusamus consectetur eius corporis eos molestias? Veritatis dicta aut eum dolore expedita laudantium!")
